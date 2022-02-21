761ff996bd65cb53471fa218b542ddde468700a3
KaiserSov <sara310105@gmail.com>
2022-02-20 19:15:17 -0500

    Modelo (entidad) del empleado

---
+++ src/main/java/com/sofka/demo/model/Employee.java
@@ -0,0 +1,97 @@
+package com.sofka.demo.model;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 25,nullable = false)
+    private String firstName;
+
+    @Column(length = 35,nullable = false)
+    private String lastName;
+
+    @Column(length = 10,nullable = false, unique = true)
+    private String employeeid;
+
+    //Constructor
+    public Employee(){
+
+    }
+
+    public Employee(String firstName, String lastName, String employeeid){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeid = employeeid;
+    }
+
+    //Getters and Setters
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeid(){
+        return employeeid;
+    }
+
+    public void setEmployeeid(String employeeid){
+        this.employeeid = employeeid;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+        }
+    @Override
+    public String toString(){
+        return "Employeed [employeedid = " + employeeid + ", firstname = " + firstName + ", id = " + id + ", lastName = " + lastName + "]";
+    }
+}
+
