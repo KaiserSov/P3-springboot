f807325338001b1242b6576a601d9f6ba7b1e099
KaiserSov <sara310105@gmail.com>
2022-02-21 15:50:45 -0500

    Agregué listas a los "Empleados" en el repositorio

--- src/main/java/com/sofka/demo/repository/IEmployeeJpaRepository.java
+++ src/main/java/com/sofka/demo/repository/IEmployeeJpaRepository.java
@@ -4,10 +4,19 @@ import com.sofka.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.management.relation.Role;
+import java.util.List;
+
 @Repository
 public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long>{
 
     //Selecciona los campos de la tabla Employee desde employeeid ='[paramentro]'
     Employee findByEmployeedid(String employeeid);
+
+    List<Employee> findByFirstName(String firstName);
+
+    List<Employee> findByLastName(String lastName);
+
+    List<Employee> findByRole(Role role);
 
 }
--- src/test/java/com/sofka/demo/repository/EmployeeJpaRepositoryTest.java
+++ src/test/java/com/sofka/demo/repository/EmployeeJpaRepositoryTest.java
@@ -8,24 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.management.relation.InvalidRoleInfoException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)//base de datos en memoriau
+@AutoConfigureTestDatabase (replace = AutoConfigureTestDatabase.Replace.NONE)
+ //@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)//base de datos en memoriau
 public class EmployeeJpaRepositoryTest {
     @Autowired
-    private IEmployeeJpaRepository repo;
+    private IEmployeeJpaRepository repoEmp1;
+
+    @Autowired
+    private IRoleJpaRepository repoRole;
+
+    @Autowired
+    private IProjectJpaRepository repoProj;
 
     @Test
     public void saveEmployee() {
         Employee jhon = new Employee("Jhon", "Smith", "empl123");
         Employee Susana = new Employee("Susana", "Valencia", "empl124");
 
-        repo.save(jhon);
-        repo.save(Susana);
+        repoEmp1.save(jhon);
+        repoEmp1.save(Susana);
 
-        repo.flush();
+        repoEmp1.flush();
 
-        assertEquals(2, repo.findAll().size());
+        assertEquals(2, repoEmp1.findAll().size());
     }
 }
